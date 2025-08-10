13b753b39b4526d4c1b6ceea792bb4844d2c7793
Frandevads <fran.dev.ads@gmail.com>
2025-08-10 15:54:42 +0100

    alteração nas urls, html, models...

--- medicos/admin.py
+++ medicos/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Medico, Especialidade
 
-# Register your models here.
+admin.site.register(Medico)
+admin.site.register(Especialidade)
--- medicos/views.py
+++ medicos/views.py
@@ -1,31 +1,26 @@
-
-
-import json
 from django.shortcuts import render
-from .models import Medico
-from .forms import AgendamentoConsultaForm
+from .models import Especialidade, Medico
 
-def medicos_e_especialidades(request):
-    especialidades = Medico.objects.values_list('especialidade', flat=True).distinct()
-    choices = [('', '--- Selecione uma Especialidade ---')] + [(e, e) for e in especialidades]
-    
-    form = AgendamentoConsultaForm()
-    form.fields['especialidade_desejada'].choices = choices
+def medicos_especialidades(request):
+    especialidade_id = request.GET.get('especialidade')
+    medico_id = request.GET.get('medico')
 
-    medicos_por_especialidade = {}
-    for medico in Medico.objects.all():
-        if medico.especialidade not in medicos_por_especialidade:
-            medicos_por_especialidade[medico.especialidade] = []
-        medicos_por_especialidade[medico.especialidade].append({
-            'id': medico.id,
-            'nome': medico.nome,
-            'crm': medico.crm,
-        })
-    medicos_json = json.dumps(medicos_por_especialidade)
+    especialidades = Especialidade.objects.all()
+    medicos = Medico.objects.none()  # padrão: nenhum médico
 
-    contexto = {
-        'form': form,
-        'medicos_json': medicos_json,
+    if especialidade_id:
+        medicos = Medico.objects.filter(especialidade_id=especialidade_id)
+
+    sucesso = False
+    if especialidade_id and medico_id:
+        sucesso = True
+
+    context = {
+        'especialidades': especialidades,
+        'medicos': medicos,
+        'especialidade_selecionada': especialidade_id,
+        'medico_selecionado': medico_id,
+        'sucesso': sucesso
     }
-    
-    return render(request, 'medicos/medicos_especialidades.html', contexto)
+
+    return render(request, 'medicos/medicos_especialidades.html', context)
